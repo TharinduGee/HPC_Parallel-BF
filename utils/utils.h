1fd756ac9f79ccc896eb1abaea982d6ac5ee365d
Tharindu <tharindugeethanjan@gmail.com>
2025-06-16 12:46:22 +0530

    compatibility with both c & c++

--- utils/utils.h
+++ utils/utils.h
@@ -7,7 +7,15 @@ typedef struct {
     int weight;
 } Edge;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 Edge* generateEdges(int vertexCount, int edgeCount, int minWeight, int maxWeight);
 void printEdges(Edge* edges, int edgeCount);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
